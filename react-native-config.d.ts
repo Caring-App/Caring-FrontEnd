cfbc1f067b9e3a97856c71f1e6f8c9998ca4e975
chiyyy <hyein031114@gmail.com>
2026-08-26 05:01:20 +0900

    chore: 미사용 @react-native-google-signin/google-signin 패키지 제거

--- react-native-config.d.ts
+++ react-native-config.d.ts
@@ -1,6 +1,5 @@
 declare module 'react-native-config' {
   export interface NativeConfig {
-    GOOGLE_WEB_CLIENT_ID?: string;
     NAVER_CLIENT_ID?: string;
     NAVER_CLIENT_SECRET?: string;
     API_BASE_URL?: string;
